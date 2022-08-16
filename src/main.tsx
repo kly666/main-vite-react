ca6c8f8255c587f0f307d7c198e6039abd1d9d48
shenquanbin <shenquanbin@58.com>
2022-08-16 17:51:59 +0800

    feat: 接入qiankun

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import { registerMicroApps, start } from 'qiankun'
+registerMicroApps([
+  {
+    name: 'my-vite-react',
+    entry: '//localhost:3002',
+    container: '#main-app',
+    activeRule: '/sub-app-vite',
+  }
+]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+start();
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('main-root')
 )
