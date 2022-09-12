462e168c3c9ce1128881fca44fb329b02a604b8c
kly <1141474965@qq.com>
2022-09-12 23:59:23 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { getStyle } from './utils/color-fade'
+import antClass from './ant-class'
 
 function App() {
-  const [count, setCount] = useState(0)
+  /** 方案一 */
+  const bodyDOM = document.querySelector('body')
+  let styleDOM = document.getElementById('ant-class')
+  if (!styleDOM && bodyDOM) {
+    const styleDOM = document.createElement('style')
+    styleDOM.id = 'ant-class'
+    styleDOM.innerHTML = antClass
+    bodyDOM.appendChild(styleDOM)
+  }
+
+  /** 方案二 */
+  // const color = getStyle('ant', {
+  //   primaryColor: 'blue'
+  // })
+  // const headDOM = document.querySelector('head')
+  // let styleDOM = document.getElementById('ant-variables-color')
+  // if (!styleDOM && headDOM) {
+  //   const styleDOM = document.createElement('style')
+  //   styleDOM.id = 'ant-variables-color'
+  //   styleDOM.innerHTML = color
+  //   headDOM.appendChild(styleDOM)
+  // }
 
   return (
     <div className="App">
       <div id="main-app">
+        <h1>我是主页面</h1>
       </div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      },
+      plugin: {
+
+      }
+    }
+  },
   server: {
     port: 3001
   }
