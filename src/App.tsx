50eef869085c9c430d0657d2fb917cf9cc5a25e0
shenquanbin <shenquanbin@58.com>
2022-09-19 00:00:23 +0800

    feat: 提取antd样式

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,27 @@ import antClass from './ant/ant-class'
 
 function App() {
   /** 方案一 */
-  const bodyDOM = document.querySelector('body')
-  let styleDOM = document.getElementById('ant-class')
-  if (!styleDOM && bodyDOM) {
-    const styleDOM = document.createElement('style')
-    styleDOM.id = 'ant-class'
-    styleDOM.innerHTML = antClass
-    bodyDOM.appendChild(styleDOM)
-  }
+  // const bodyDOM = document.querySelector('body')
+  // let styleDOM = document.getElementById('ant-class')
+  // if (!styleDOM && bodyDOM) {
+  //   const styleDOM = document.createElement('style')
+  //   styleDOM.id = 'ant-class'
+  //   styleDOM.innerHTML = antClass
+  //   bodyDOM.appendChild(styleDOM)
+  // }
 
   /** 方案二 */
-  // const color = getStyle('ant', {
-  //   primaryColor: 'blue'
-  // })
-  // const headDOM = document.querySelector('head')
-  // let styleDOM = document.getElementById('ant-variables-color')
-  // if (!styleDOM && headDOM) {
-  //   const styleDOM = document.createElement('style')
-  //   styleDOM.id = 'ant-variables-color'
-  //   styleDOM.innerHTML = color
-  //   headDOM.appendChild(styleDOM)
-  // }
+  const color = getStyle('ant', {
+    primaryColor: 'blue'
+  })
+  const headDOM = document.querySelector('head')
+  let styleDOM = document.getElementById('ant-variables-color')
+  if (!styleDOM && headDOM) {
+    const styleDOM = document.createElement('style')
+    styleDOM.id = 'ant-variables-color'
+    styleDOM.innerHTML = color
+    headDOM.appendChild(styleDOM)
+  }
 
   return (
     <div className="App">
